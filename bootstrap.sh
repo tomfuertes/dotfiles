61b8c630672341a56975f0b832a7720d8507727d
Tom Fuertes <tomfuertes@gmail.com>
2021-07-07 21:00:17 -0400

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,17 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+  if [ -n "$ZSH_VERSION" ]; then
+    source ~/.zshrc;
+  elif [ -n "$BASH_VERSION" ]; then
+    source ~/.bash_profile;
+  else
+    echo 'unknown shell'
+  fi
+
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ false ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
